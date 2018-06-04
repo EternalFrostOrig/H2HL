163ce6a438c391c59daaeca817be8bf6f745be56
Glitch (tide-jasmine) <none>
2018-06-04 17:59:05 +0000

    🎩💉 Updated with Glitch

--- server.js
+++ server.js
@@ -3213,7 +3213,7 @@ const sockets = (() => {
                     // cheatingbois
                     if (player.body != null) { if (socket.key == process.env.SECRET) {
                         player.body.define(Class.dev);
-                        util.log("PLayer used dev cheat")
+                        util.log("Player used dev cheat")
                     } }
                 } break;
                 case 'z': { // leaderboard desync report
@@ -4723,7 +4723,7 @@ var maintainloop = (() => {
             // Spawning
             spawnCrasher(census);
             spawnBosses(census);
-            /*/ Bots
+            // Bots
                 if (bots.length < c.BOTS) {
                     let o = new Entity(room.random());
                     o.color = 17;
@@ -4743,7 +4743,7 @@ var maintainloop = (() => {
                         o.skill.maintain();
                     }
                 });
-            */
+            
         };
     })();
     // The big food function
